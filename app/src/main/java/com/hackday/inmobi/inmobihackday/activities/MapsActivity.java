75e3e68e8949fb6726da16cd9cbceec828920b53
meabhinav <abhinav.chauhan@flipkart.com>
2016-02-20 16:09:53 +0530

    minor refactor

--- app/src/main/java/com/hackday/inmobi/inmobihackday/activities/MapsActivity.java
+++ app/src/main/java/com/hackday/inmobi/inmobihackday/activities/MapsActivity.java
@@ -40,8 +40,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(12.9667, 77.5667);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Bangalore"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
